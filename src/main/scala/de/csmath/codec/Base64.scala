39b7a381acb7d9cdd33bffebeded115b9f2adbcf
Lothar Pfeiler <lpfeiler@Lothars-MBP.fritz.box>
2016-05-24 19:31:10 +0200

    added encode for String

--- src/main/scala/de/csmath/codec/Base64.scala
+++ src/main/scala/de/csmath/codec/Base64.scala
@@ -2,7 +2,55 @@ package de.csmath.codec;
 
 
 object Base64 {
-    
+
+    val fill: Byte = 0
+    val fillPad: Byte = 64
+
+    val chars = Array( ((65 to 90) map (_.toChar)).toArray,
+                       ((97 to 122) map (_.toChar)).toArray,
+                       ((48 to 57) map (_.toChar)).toArray,
+                       Array('+','/','=')).flatten
+
     def encode(data: Traversable[Byte], enc: String) = ???
-    
+
+    def encodeTriple(x: (Byte,Byte,Byte), pad: Boolean, fill: Int) = {
+        val (a,b,c) = x
+        val ia: Int = if (a < 0) 256 + a else a
+        val ib: Int = if (b < 0) 256 + b else b
+        val ic: Int = if (c < 0) 256 + c else c
+        val r = (ia >> 2).toByte
+        val s = (((ia & 3) << 4) ^ (ib >> 4)).toByte
+        val t = if (pad && fill == 2)
+                   fillPad
+                   else (((ib & 15) << 2) ^ (ic >> 6)).toByte
+        val u = if (pad && fill > 0)
+                   fillPad
+                   else (ic & 63).toByte
+        Array(chars(r),chars(s),chars(t),chars(u))
+    }
+
+    def l2t(a: Array[Byte]) = a.length match {
+       case 1 => (a(0), fill, fill)
+       case 2 => (a(0), a(1), fill)
+       case 3 => (a(0), a(1), a(2))
+    }
+
+    def encode(s: String, enc: String) = {
+        val bytes = s.getBytes
+
+        val padsNeeded = (3 - bytes.length % 3) % 3
+
+        val triples = bytes grouped 3 map (l2t(_))
+
+        val builder = StringBuilder.newBuilder
+
+        for (t <- triples) {
+            if (triples.hasNext)
+                builder appendAll encodeTriple(t,false,0)
+            else
+                builder appendAll encodeTriple(t,true,padsNeeded)
+        }
+        builder.toString
+    }
+
 }
--- src/test/scala/Base64Spec.scala
+++ src/test/scala/Base64Spec.scala
@@ -1,7 +1,14 @@
 import org.scalatest._
+import de.csmath.codec.Base64._
 
 class Base64Spec extends FlatSpec with Matchers {
   "Base64" should "encode to base64" in {
-    true should === (true)
+      encode("", "") should === ("")
+      encode("f", "") should === ("Zg==")
+      encode("fo", "") should === ("Zm8=")
+      encode("foo", "") should === ("Zm9v")
+      encode("foob", "") should === ("Zm9vYg==")
+      encode("fooba", "") should === ("Zm9vYmE=")
+      encode("foobar", "") should === ("Zm9vYmFy")
   }
 }
