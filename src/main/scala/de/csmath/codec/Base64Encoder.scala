86ac6155f5d9e8e1775fcbb7e06e07723d29d961
Lothar Pfeiler <lpfeiler@Lothars-MacBook-Pro.fritz.box>
2016-05-25 21:33:36 +0200

    percEncode added

--- src/main/scala/de/csmath/codec/Base64Encoder.scala
+++ src/main/scala/de/csmath/codec/Base64Encoder.scala
@@ -23,8 +23,6 @@ object Base64Encoder {
                 enc == BASE64URL ||
                 enc == BASE64URLNOPAD)
 
-        val padsNeeded = (3 - data.size % 3) % 3
-
         val needPads = enc == BASE64 || enc == BASE64URL
         val chars: Vector[Char] = if (enc == BASE64URL || enc == BASE64URLNOPAD)
                                      urlChars
@@ -35,21 +33,28 @@ object Base64Encoder {
         val builder = StringBuilder.newBuilder
 
         for (t <- triples) {
-            val filled = if (triples.hasNext) 0 else padsNeeded
-            builder appendAll encodeTriple(t,needPads,filled,chars)
+            builder appendAll encodeTriple(t,needPads,chars)
         }
-        if (enc == BASE64URL)
-            builder replace (builder.length - padsNeeded, builder.length, "%3D"*padsNeeded)
+        
+        if (enc == BASE64URL) percEncode(builder)
 
         builder.toString
+    }
+
+    private def percEncode(b: StringBuilder) {
+        val len = b.length
+        val pads = if ( len > 1 && (b charAt (len-2)) == '=') 2
+                   else if ( len > 0 && (b charAt (len-1)) == '=') 1
+                   else 0
+        b replace (len - pads, len, "%3D"*pads)
     }
 
     /**
      * encodeTriple: (Array[Int],Boolean,Int) => Array[Char]
      */
-    def encodeTriple(x: Array[Int], needPad: Boolean, fill: Int, chars:Vector[Char]) = {
-        require(x.length == 3)
-        val Array(a,b,c) = x
+    def encodeTriple(x: Array[Int], needPad: Boolean, chars:Vector[Char]) = {
+        require(x.length == 4)
+        val Array(fill,a,b,c) = x
         val r = (a >> 2)
         val s = (((a & 3) << 4) ^ (b >> 4))
         val t = if (fill == 2) pad
@@ -68,7 +73,7 @@ object Base64Encoder {
     private val b2i = (a: Array[Byte]) => {
        var ai = for (i <- a) yield if (i < 0) 256 + i else i.toInt
        while (ai.length < 3) ai = ai :+ fill
-       ai
+       (3-a.length) +: ai
     }
 
     /**
