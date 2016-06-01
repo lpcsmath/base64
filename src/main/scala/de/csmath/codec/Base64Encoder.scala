357cc7ebd90b76214d24edff6ec78e0d7e15ec4f
Lothar Pfeiler <lpfeiler@Lothars-MBP.fritz.box>
2016-06-01 17:10:11 +0200

    added comments

--- src/main/scala/de/csmath/codec/Base64Encoder.scala
+++ src/main/scala/de/csmath/codec/Base64Encoder.scala
@@ -3,17 +3,35 @@ package de.csmath.codec;
 import Codec._
 
 
+/**
+ *  The class Base64Encoder provides operations to encode a
+ *  traversable collection of bytes.
+ */
 class Base64Encoder extends BaseNEncoder {
 
+    /**
+     *  The size of the elements of bytes which need to be encoded together.
+     */
     val groupSize = 3
 
+    /**
+     *  The code alphabet.
+     */
     override val chars = Vector() ++ (((65 to 90) map (_.toByte)) ++
                                     ((97 to 122) map (_.toByte)) ++
                                     ((48 to 57) map (_.toByte)) :+
                                     '+'.toByte :+ '/'.toByte )
 
+
     /**
-     * encodeTriple: (Array[Int],Boolean,Int) => Array[Char]
+     *  Encodes a group of Integers. The number of elements of the groupis
+     *  determined by the value of the field groupSize.
+     *  @param x       The pair (fill,seq) with fill being the number of zero-elements
+     *                 added to the group to get enough elements for encoding and seq
+     *                 being the group of Integers to be encoded.
+     *  @param needPad true if the filled zero-elements need to be encoded to
+     *                 pad-elements and false if they can be omitted.
+     *  @return        A sequence of endoded Bytes
      */
     override def encodeGroup(x: (Int,Seq[Int]), needPad: Boolean) = {
         val (fill,Seq(a,b,c)) = x
