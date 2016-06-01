5a91298e2ac87fec75ea85dde2d3c9f04ac796e4
Lothar Pfeiler <lpfeiler@Lothars-MacBook-Pro.fritz.box>
2016-06-01 12:06:43 +0200

    added comments to Stream*

--- src/main/scala/de/csmath/codec/StreamDecoder.scala
+++ src/main/scala/de/csmath/codec/StreamDecoder.scala
@@ -2,8 +2,22 @@ package de.csmath.codec
 
 import scala.util._
 
+/**
+ *  This trait provides stream operations for byte oriented decoders.
+ */
 trait StreamDecoder extends StreamCodec {
 
+    /**
+     *  Creates a stream of bytes inside a <em>Success</em> constructor if each
+     *  of the items of the given traversable collection fullfill the given
+     *  predicate.
+     *  @param pred The predicate to ensure a certain property of each byte.
+     *  @param data The traversable collection of bytes.
+     *  @return     <em>Success(stream)</em> with <em>stream</em> being a
+     *              stream of bytes where <em>pred</em> holds for each item
+     *              or <em>Failure(e)</em> with <em>e</em> being an
+     *              IllegalArgumentException.
+     */
     protected def checkedByteStream(pred: (Byte => Boolean), data: Traversable[Byte]): Try[Stream[Byte]] =
         if (data.isEmpty) Success(Stream.empty)
         else
@@ -13,6 +27,14 @@ trait StreamDecoder extends StreamCodec {
                 Failure( new IllegalArgumentException("Data contains illegal bytes."))
 
 
+    /**
+     *  This is small helper function to filter a byte stream according
+     *  to a given predicate.
+     *  @param pred The predicate to ensure a certain property of each byte.
+     *  @param s    The stream to be filtered.
+     *  @return     The remaining bytes that fullfill the predicate.
+     */
     protected def filteredStream(pred: (Byte => Boolean), s:Stream[Byte]) =
         s filter pred
+        
 }
--- src/main/scala/de/csmath/codec/StreamEncoder.scala
+++ src/main/scala/de/csmath/codec/StreamEncoder.scala
@@ -2,8 +2,17 @@ package de.csmath.codec
 
 import scala.util._
 
+/**
+ *  This trait provides stream operations for byte oriented encoders.
+ */
 trait StreamEncoder extends StreamCodec {
 
+    /**
+     *  Creates an <em>Int</em> stream out of a byte stream where each
+     *  integer is non negative.
+     *  @param t A traversable collection of bytes.
+     *  @return  A stream of non negative integers.
+     */
     protected def intStream(t: Traversable[Byte]): Stream[Int] = {
         if (t.isEmpty) Stream.empty
         else {
